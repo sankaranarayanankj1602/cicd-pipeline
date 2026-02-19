81767e7e089f6bb083a3f74478491e4875c353fa
sankaranarayanankj1602 <sankaranarayanan.kj@ignitarium.com>
2026-02-19 12:11:37 +0530

    Updated Makefile and C File

--- Makefile
+++ Makefile
@@ -1,1 +1,15 @@
-gcc -o first_job sample_file.c
+# Compiler and flags
+CC = gcc
+CFLAGS = -Wall -g
+
+# Target executable
+TARGET = first_job
+
+
+# Compile main.c
+main.o: sample_file.c
+	$(CC) $(CFLAGS) -c sample_file.c
+
+# Clean up build files
+clean:
+	rm -f *.o $(TARGET)
--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 print("CI/CD Pipeline demo")
+
+# x=input("Enter the text input")
+
+# # print("Test Input entered",x)
+
+# y=int(input("Enter the Number as input"))
+
+# # print("Number input",y)
+
+# with open('test.txt','a+') as file:
+#     file.write("Test Input entered \t"+x)
+
+#     file.write("\nNumber input \t"+str(y))
+
+#     file_read = file.read()
+
+#     print(file_read)
